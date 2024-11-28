08eea61234c6b747c75b5439d0566af6872ecf3a
MicaOrtega <micaelaortega13@gmail.com>
2024-11-28 00:57:36 -0300

    Inicial

---
+++ accounts/admin.py
@@ -0,0 +1,44 @@
+from django.contrib import admin
+from .models import CustomUser
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+# Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    list_display = (
+        "username",
+        "email",
+        "is_active",
+        "is_staff",
+        "is_superuser",
+    )
+    
+    add_fieldsets = (
+        (None, {
+            'fields': (
+                'username',
+                'email',
+                'password1',
+                'password2',
+            )
+        }),
+    )
+    
+    # fieldsets = (
+    #     (None, {'fields': (
+    #         'username',
+    #         'email',
+    #         'age',
+    #         'password'
+    #     )}),
+    #     ('Permisos', {'fields': (
+    #         'is_active',
+    #         'is_staff',
+    #         'is_superuser',
+    #         'groups',
+    #         'user_permissions'
+    #     )}),
+    # )
+    # search_fields = ('username', 'email')
+
+admin.site.register(CustomUser)
